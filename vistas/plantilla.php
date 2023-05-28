25cef23911f663033eb044168ea674bd79364669
Belen Cantarini <belencantarini@gmail.com>
2023-05-28 20:13:20 -0300

    actualizacion texto

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="./vistas/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Poppins:wght@300;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -17,7 +20,7 @@
             <a class="navbar-brand flex-wrap align-items-center justify-content-center d-flex d-md-none" href="index.php?ruta=inicio">
                 <img src="./vistas/imagenes/logo.jpg" alt="Logo" height="auto" width="100" class="d-inline-block">
                 <div class="d-flex flex-column">
-                    <p class="h4 mb-0">Administración Dinatale</p>
+                    <p class="h4 mb-0 text-uppercase titulo">Administración Dinatale</p>
                     <p class="mb-0">Consorcios</p>
                 </div>
             </a>
@@ -27,7 +30,7 @@
                 <a class="navbar-brand flex-wrap justify-content-center align-items-center d-none d-md-flex" href="index.php?ruta=inicio">
                     <img src="./vistas/imagenes/logo.jpg" alt="Logo" height="auto" width="100" class="d-inline-block">
                     <div class="d-flex flex-column">
-                        <p class="h4 mb-0">Administración Dinatale</p>
+                        <p class="h4 mb-0 text-uppercase titulo">Administración Dinatale</p>
                         <p class="mb-0">Consorcios</p>
                     </div>
                 </a>
