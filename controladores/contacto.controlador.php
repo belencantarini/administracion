cdbd311aec3a957a4ab77a555321ce67f2a74635
Belen Cantarini <belencantarini@gmail.com>
2023-06-03 12:30:38 -0300

    inicio y contacto

--- controladores/contacto.controlador.php
+++ controladores/contacto.controlador.php
@@ -1,15 +1,17 @@
 <?php
 
 // ENVIO DE MAIL
+
+
     $nombre_form = $_POST['nombre'];
     $email_form = $_POST['email'];
     $mensaje_form = $_POST['mensaje'];
     $cuerpo_mensaje = "Nombre: " . $nombre_form . "\r\n" . "Email: " . $email_form . "\r\n" . "Mensaje: " . $mensaje_form;
 
-mail("damian.gdl@hotmail.com", "Mensaje enviado desde mi sitio web", $cuerpo_mensaje);
 
-
-// NOTIFICACION ENVIO DE MENSAJE
-
-
-header("Location: ../index.php?ruta=contacto&exito_envio");
+if(mail("administraciondinatale@gmail.com", "Mensaje enviado desde mi sitio administraciondinatale.com.ar", $cuerpo_mensaje))
+{   
+    header("Location: ../index.php?ruta=contacto&exito_envio");
+}else{
+    header("Location: ../index.php?ruta=contacto&error_envio");
+}
--- controladores/sweetalert.controlador.php
+++ controladores/sweetalert.controlador.php
@@ -5,31 +5,36 @@ function exitoEnvioContactoSweetAlert(){
     Swal.fire({
         icon: 'success',
         title: 'Mensaje enviado!',
-        text: 'Hemos recibido tu mensaje, te contestaremos a la brevedad.',
+        text: 'Gracias por contactarnos, te responderemos a la brevedad.',
         confirmButtonText:'OK',
         confirmButtonColor: '#3085d6',
     })
     </script>";
 }
 
-function exitoEnvioPresupuestoSweetAlert(){
+function errorEnvioContactoSweetAlert(){
     echo "<script>
     Swal.fire({
-        icon: 'success',
-        title: 'Mensaje enviado!',
-        text: 'Hemos recibido tu pedido de presupuesto, te contestaremos a la brevedad.',
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Hubo un error al enviar el mensaje, por favor intenta nuevamente.',
         confirmButtonText:'OK',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = 'index.php?ruta=presupuesto'
-        } 
+        confirmButtonColor: '#3085d6',
     })
     </script>";
 }
 
 function redireccionContacto(){
     echo "<script>
-    window.history.replaceState('http://localhost/proyectos%20php/AdministracionDiNatale/', '', 'index.php?ruta=contacto');
+    window.history.replaceState(null, '', 'index.php?ruta=contacto');
     </script>";
 }
+
+
+
+
+
+
+
+
 
