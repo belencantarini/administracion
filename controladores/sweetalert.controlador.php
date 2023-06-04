f14102aeefb458fb962f5064e3daa9892f0ebc4b
Belen Cantarini <belencantarini@gmail.com>
2023-06-04 20:34:22 -0300

    letra

--- controladores/sweetalert.controlador.php
+++ controladores/sweetalert.controlador.php
@@ -5,7 +5,7 @@ function exitoEnvioContactoSweetAlert(){
     Swal.fire({
         icon: 'success',
         title: 'Mensaje enviado!',
-        text: 'Gracias por contactarnos, te responderemos a la brevedad.',
+        text: 'Gracias por contactarnos, le responderemos a la brevedad.',
         confirmButtonText:'OK',
         confirmButtonColor: '#3085d6',
     })
@@ -17,7 +17,7 @@ function errorEnvioContactoSweetAlert(){
     Swal.fire({
         icon: 'error',
         title: 'Oops...',
-        text: 'Hubo un error al enviar el mensaje, por favor intenta nuevamente.',
+        text: 'Hubo un error al enviar el mensaje, por favor intente nuevamente.',
         confirmButtonText:'OK',
         confirmButtonColor: '#3085d6',
     })
